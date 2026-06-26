f0858743ed9dca443f897c16f0f775853d1077fe
Andrew Sepic <andrew.sepic@mapbox.com>
2026-06-26 14:08:38 -0400

    Finished processing script for metros

--- scripts/data-prep.ts
+++ scripts/data-prep.ts
@@ -26,79 +26,78 @@ type USCityProperties = {
     population: number;
 }
 
-type CityFeature = {
-    type: "Feature",
-    properties: USCityProperties,
-    geometry: {
-        type: "Point",
-        coordinates: [number, number]
-    }
-}
+// type CityFeature = {
+//     type: "Feature",
+//     properties: USCityProperties,
+//     geometry: {
+//         type: "Point",
+//         coordinates: [number, number]
+//     }
+// }
 
 ( async () => {
 
-    const MAPBOX_ACCESS_TOKEN = "YOUR_MAPBOX_ACCESS_TOKEN"
+    const MAPBOX_ACCESS_TOKEN = process.env.VITE_MAPBOX_ACCESS_TOKEN;
 
     const __dirname = import.meta.dirname
-    const csvFilePath = path.resolve(__dirname, '../data/testMetros.csv')
+    const csvFilePath = path.resolve(__dirname, '../data/metros.csv')
 
     const csvContent = fs.readFileSync(csvFilePath, { encoding: 'utf-8' })
     const toNumber = (s: string) => Number(s.replace(/[$,]/g, ''))
 
-    parse(csvContent, {
-        delimiter: ',',
-        columns: true
-    }, (error, result: CsvCityData[]) => {
-        if(error) {
-            console.error(error)
-        }
+    const metroGeojson = await new Promise((resolve, reject) => {
 
-        const cities: CityFeature[] = await result.map(async (c) =>  {
-            const [city, state ] = c.Metro.split(',')
-
-            // {
-            // "type": "FeatureCollection",
-            // "features": [
-               
-            // ]
-            // }
-                    
-            const singleCity:USCityProperties = {
-                rank: toNumber(c.Rank),
-                name: city,
-                state: state,
-                newHomesPer1K22: toNumber(c['New housing units authorized per 1k existing homes (2022)']),
-                totalNewHomes22: toNumber(c['Total new housing units authorized (2022)']),
-                newHomesPer1k24: toNumber(c['New housing units authorized per 1k existing homes (2024)']),
-                totalNewHomes24: toNumber(c['Total new housing units authorized (2024)']),
-                medianPrice: toNumber(c['Median home price']),
-                population: toNumber(c.Population)
+       parse(csvContent, {
+            delimiter: ',',
+            columns: true
+        }, (error, result: CsvCityData[]) => {
+            if(error) {
+                console.error(error)
             }
 
-            // Geocode City & State
-            let coordinates
-            try {
-                const structuredQuery = new URLSearchParams({place: city, region: state}).toString();
-                const geocode = await fetch(`https://api.mapbox.com/search/geocode/v6/forward?q=${structuredQuery}&access_token=${MAPBOX_ACCESS_TOKEN}`)
-                const result = await geocode.json();
-                coordinates = result.features[0].geometry.coordinates
-                console.log(city, state, ':', coordinates)
-            } catch(err) {
-                console.error(err)
-            }
-            
-            const feature:CityFeature =  {
-                "type": "Feature",
-                "properties": singleCity,
-                "geometry": {
-                    "type": "Point",
-                    "coordinates": coordinates
+            Promise.all(result.map(async (c) =>  {
+                const [city, state] = c.Metro.split(',')
+ 
+                const singleCity:USCityProperties = {
+                    rank: toNumber(c.Rank),
+                    name: city,
+                    state: state.trim(),
+                    newHomesPer1K22: toNumber(c['New housing units authorized per 1k existing homes (2022)']),
+                    totalNewHomes22: toNumber(c['Total new housing units authorized (2022)']),
+                    newHomesPer1k24: toNumber(c['New housing units authorized per 1k existing homes (2024)']),
+                    totalNewHomes24: toNumber(c['Total new housing units authorized (2024)']),
+                    medianPrice: toNumber(c['Median home price']),
+                    population: toNumber(c.Population)
                 }
-            }
 
-            cities.push(feature)
+                // Geocode City & State
+                let coordinates
+                try {
+                    const structuredQuery = new URLSearchParams({place: city, region: state}).toString();
+                    const geocode = await fetch(`https://api.mapbox.com/search/geocode/v6/forward?q=${structuredQuery}&access_token=${MAPBOX_ACCESS_TOKEN}`)
+                    const result = await geocode.json();
+                    coordinates = result.features[0].geometry.coordinates
+                    console.log(city, state, ':', coordinates)
+                } catch(err) {
+                    reject(err)
+                }
+                
+                return  {
+                    "type": "Feature",
+                    "properties": singleCity,
+                    "geometry": {
+                        "type": "Point",
+                        "coordinates": coordinates
+                    }
+                }
+            })).then(cities => resolve(cities))
         })
+    })
+    console.log("metroGeojson", metroGeojson)
 
+    fs.writeFile('./data/metros.json', JSON.stringify(metroGeojson, null, 2), (err) => {
+        if(err) console.error(err);
+        else console.log("Wrote Cities geojson")
     })
 })()
 
